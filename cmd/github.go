f10d41d8666a2aea9d5aae066da2fa7250075da4
Michal Franc <lam.michal.franc@gmail.com>
2018-07-31 21:21:57 +0100

    Small cleanup simplifing code

--- cmd/github.go
+++ cmd/github.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+// Copyright © 2018 Michal Franc
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -42,45 +42,29 @@ func getLocalGitURL() string {
 // githubCmd represents the github command
 var githubCmd = &cobra.Command{
 	Use:   "github",
-	Short: "Opens up a default browser with an url to github repo - in directory.",
-	Long: `It uses xdg-open to use default browser.
-	
-	A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Opens up a default browser with master remote url for github.",
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Printf("Opening github repo url in the browser")
 
 		url := getLocalGitURL()
 
-		if len(url) > 0 {
-			openBrowserCommand := exec.Command("xdg-open", url)
-			err := openBrowserCommand.Run()
-
-			if err != nil {
-				log.Printf("Command finished with error: %v", err)
-			}
+		if len(url) <= 0 {
+			log.Printf("Github remote url empty cannot open browser")
 			return
 		}
 
-		log.Printf("Github remote url empty cannot open browser")
+		openBrowserCommand := exec.Command("xdg-open", url)
+		err := openBrowserCommand.Run()
+
+		if err != nil {
+			log.Printf("Command finished with error: %v", err)
+		}
+
 		return
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(githubCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// githubCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// githubCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
